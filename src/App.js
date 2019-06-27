9b4b30dbfec03d8a13efe3fb6c19d30cf868dbee
mhmdbehairy <mhmd.behairy@gmail.com>
2019-06-27 08:37:50 +0200

    Task Completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Editor } from '@tinymce/tinymce-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  handleEditorChange = (e) => {
+    console.log('Content was updated:', e.target.getContent());
+  }
+
+  render() {
+    return (
+      <Editor
+        apiKey='ig7dylw49uiphxdccqt3h7gifwrbkuckffqdhg6wby9kqux8'
+        initialValue="<p>This is the initial content of the editor</p>"
+        init={{
+          plugins: 'link image code textpattern lists advlist',
+          textpattern_patterns: [
+            { start: '- ', cmd: 'InsertUnorderedList', value: { 'list-style-type': 'circle' } },
+            { start: '* ', cmd: 'InsertUnorderedList', value: { 'list-style-type': 'disc' } },
+            { start: '# ', cmd: 'InsertUnorderedList', value: { 'list-style-type': 'square' } },
+          ],
+          setup: function (editor) {
+            editor.addShortcut('ctrl+r', 'UnorderedList', function () {
+              editor.execCommand('InsertUnorderedList');
+            });
+          },
+          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code | bullist'
+        }}
+        onChange={this.handleEditorChange}
+      />
+    );
+  }
 }
 
 export default App;
